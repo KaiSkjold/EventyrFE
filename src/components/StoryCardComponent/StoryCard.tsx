198c3253dafb5a3255b2c576be06fde85652047d
KaiSkjold <skjoldann@gmail.com>
2025-11-03 08:45:02 +0100

    added favorite icon to card

--- src/components/StoryCardComponent/StoryCard.tsx
+++ src/components/StoryCardComponent/StoryCard.tsx
@@ -1,13 +1,26 @@
+import { useState } from 'react'
+import Heart from '../svgComponents/Heart'
 import './StoryCard.css'
 
 const StoryCard = () => {
+
+  const [isFavorite, setIsFavorite] = useState<'filled' | 'outlined'>('outlined')
+  const handleFavoriteClick = () => {
+    setIsFavorite(isFavorite === 'filled' ? 'outlined' : 'filled')
+  }
+
   return (
-    <div className="story-card">
-      <div className="story-title">
-        <h2>Story Title</h2>
+    <div className="card-container">
+      <div className="favorite" onClick={handleFavoriteClick}>
+        <Heart heartType={isFavorite}/>
       </div>
-      <div className="img"><p>Image</p></div>
-      <div className="description"><p>Description</p></div>
+      <div className="card-main">
+        <div className="card-top">
+          <h2>Story Title</h2>
+        </div>
+        <div className="card-img"><img src="https://picsum.photos/300/200" alt="Story Cover Image"/></div>
+        <div className="card-description"><p>Description</p></div>
+      </div>
     </div>
   )
 }
